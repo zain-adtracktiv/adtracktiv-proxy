f8c0c1a1fff364d7ea025f4484652a3b672d759c
Zain <zain@adtracktiv.io>
2024-05-20 09:20:36 +0500

    feat: added init endpoint

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,6 @@
 import { Client } from '@neondatabase/serverless';
 import { Router } from 'itty-router';
-import { parse } from 'cookie';
+import { parse, serialize } from 'cookie';
 
 // Create a new router
 const router = Router();
@@ -49,34 +49,72 @@ router.get('/r/*', async (request, env, ctx) => {
 router.post('/e', async (request, env, ctx) => {
 	const body = await request.json();
 
-	const url = new URL(body.location);
-	const isRotatorUrl = url.pathname.startsWith('/r/');
-
-	const cookie = parse(request.headers.get('Cookie') || '');
-
-	let redirectUrl;
-	if (isRotatorUrl) {
-		redirectUrl = cookie['redirectUrl'];
-	}
-
-	const variations = JSON.parse(cookie['variations']);
-	const variation = variations ? variations.find((v) => v.url === (redirectUrl || body.location)) : null;
-
 	const client = new Client(env.DATABASE_URL);
 	await client.connect();
 
-	await client.query(`INSERT INTO event (name, location, rotator_url, page_variation_name) VALUES ($1, $2, $3, $4)`, [
-		body.eventName,
-		redirectUrl || body.location,
-		redirectUrl && body.location,
-		variation?.name,
-	]);
+	const cookie = parse(request.headers.get('Cookie') || '');
+
+	const al = JSON.parse(cookie['al'] || '{}');
+	const userId = al?.userId;
+
+	for (const event of body) {
+		const url = new URL(event.location);
+		const isRotatorUrl = url.pathname.startsWith('/r/');
+
+		// redirectUrl is the url to which the rotator redirects the user to
+		let redirectUrl;
+		if (isRotatorUrl) {
+			redirectUrl = cookie['redirectUrl'];
+		}
+
+		const variations = JSON.parse(cookie['variations'] || '[]');
+		const variation = variations ? variations.find((v) => v.url === (redirectUrl || event.location)) : null;
+
+		await client.query(`INSERT INTO event (name, location, rotator_url, page_variation_name, user_id) VALUES ($1, $2, $3, $4, $5)`, [
+			event.eventName,
+			redirectUrl || event.location,
+			redirectUrl && event.location,
+			variation?.name,
+			userId,
+		]);
+	}
 
 	ctx.waitUntil(client.end());
 
 	return Response.json({
 		success: true,
 	});
+});
+
+router.post('/i', async (request, env, ctx) => {
+	const origin = request.headers.get('origin');
+	const eventsConfig = await env.ADTRACKTIV.get('www.marketintelgpt.com'); // replace url with origin
+
+	const cookie = parse(request.headers.get('Cookie') || '');
+	const oldAl = JSON.parse(cookie?.['al'] || '{}');
+
+	let userId = oldAl?.userId;
+	if (!userId) {
+		// also create user in database with this id
+		// userId = crypto.randomUUID();
+		userId = '3a26df84-cf3a-42d0-89fe-0a9619b65ba1';
+	}
+
+	const al = {
+		userId,
+		...oldAl,
+	};
+
+	const newCookie = serialize('al', JSON.stringify(al), {
+		httpOnly: true,
+	});
+
+	const response = Response.json({
+		success: true,
+	});
+	response.headers.append('Set-Cookie', newCookie);
+
+	return response;
 });
 
 router.all('*', (request) => {
