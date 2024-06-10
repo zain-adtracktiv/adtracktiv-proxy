a2e70b1f24899a8b8546770870dc5625c284811a
Zain <zain@adtracktiv.io>
2024-06-10 06:52:29 +0500

    feat: added reconcile endpoint

--- src/worker.js
+++ src/worker.js
@@ -2,43 +2,67 @@ import { Client } from '@neondatabase/serverless';
 import { Router } from 'itty-router';
 import { parse, serialize } from 'cookie';
 import { deepMerge, extractRootDomain } from './utils';
+import _ from 'lodash';
 
 // Create a new router
 const router = Router();
 
-// This will be replaced with the subdomain where the pages are hosted (e.g ain)
-const REDIRECT_URL = 'https://www.marketintelgpt.com';
-
 router.post('/e', async (request, env, ctx) => {
 	const body = await request.json();
+
+	// TODO: Fill in user agent and geo fields
+	// console.log(request.headers.get('user-agent'));
+	// console.log(request.headers.get('X-Forwarded-For'));
 
 	const client = new Client(env.DATABASE_URL);
 	await client.connect();
 
 	const cookie = parse(request.headers.get('Cookie') || '');
 
-	const al = JSON.parse(cookie['al'] || '{}');
-	const userId = al?.userId;
+	const linker = cookie['_al'] || '';
+	const [pseudoId, sessionId] = linker.split('*');
+
+	const marketingParams = JSON.parse(atob(sessionId.split('.')[2]));
+
+	const page = marketingParams.lp;
+	const pageUrl = new URL(page);
+	const hostname = pageUrl.hostname;
+	const pathname = pageUrl.pathname;
+
+	const referrer = request.headers.get('referer');
+	const referrerUrl = referrer ? new URL(referrer) : null;
+	const referrerHostname = referrerUrl?.hostname;
+	const referrerPathname = referrerUrl?.pathname;
 
 	for (const event of body) {
 		const url = event?.location ? new URL(event.location) : '';
-		const isRotatorUrl = url?.pathname?.startsWith('/r/');
 
+		const isRotatorUrl = url?.pathname?.startsWith('/r/');
 		// redirectUrl is the url to which the rotator redirects the user to
 		let redirectUrl;
-		if (isRotatorUrl) {
-			redirectUrl = cookie['redirectUrl'];
-		}
 
-		const variations = JSON.parse(cookie['variations'] || null);
-
-		await client.query(`INSERT INTO event (name, location, rotator_url, page_variation_name, user_id) VALUES ($1, $2, $3, $4, $5)`, [
-			event.eventName,
-			redirectUrl || event.location,
-			redirectUrl && event.location,
-			variations,
-			userId,
-		]);
+		await client.query(
+			`INSERT INTO event (name, company_id, pseudo_id, session_id, parameters, utm_campaign, utm_source, utm_medium, utm_content, utm_id, utm_term, page_href, page_hostname, page_pathname, referrer_href, referrer_hostname, referrer_pathname) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)`,
+			[
+				event.eventName,
+				event.companyId,
+				pseudoId,
+				sessionId,
+				event.parameters,
+				marketingParams.utm_campaign,
+				marketingParams.utm_source,
+				marketingParams.utm_medium,
+				marketingParams.utm_content,
+				marketingParams.utm_id,
+				marketingParams.utm_term,
+				page,
+				hostname,
+				pathname,
+				referrer,
+				referrerHostname,
+				referrerPathname,
+			]
+		);
 	}
 
 	ctx.waitUntil(client.end());
@@ -181,14 +205,173 @@ router.post('/decide', async (request, env, ctx) => {
 	return Response.json({
 		redirectUrl: experience.url,
 		variations: experience.flags,
+		// hotLinkSlug,
+		// pathwayId,
+		// experimentId,
+		// experimentVariantId,
 	});
 });
 
-router.all('*', (request) => {
-	const url = new URL(request.url);
-	const params = url.searchParams.toString();
+router.post('/pseudo-session', async (request, env, ctx) => {
+	const body = await request.json();
 
-	return fetch(`${REDIRECT_URL}/${url.pathname}${params ? `?${params}` : ''}`);
+	const client = new Client(env.DATABASE_URL);
+	await client.connect();
+
+	const firstPage = body.marketingParams.lp;
+	const firstPageUrl = new URL(body.marketingParams.lp);
+	const hostname = firstPageUrl.hostname;
+	const pathname = firstPageUrl.pathname;
+
+	const referrer = body.referrer ? new URL(body.referrer) : null;
+	const referrerHostname = referrer?.hostname;
+	const referrerPathname = referrer?.pathname;
+
+	if (body.shouldCreatePseudo) {
+		const trackedUserId = crypto.randomUUID();
+		const timestamp = body.pseudoId.split('.')[1];
+
+		await client.query(
+			`INSERT INTO tracked_user (id,
+			company_id,
+			city,
+			region,
+			country,
+			first_utm_campaign,
+			first_utm_source,
+			first_utm_medium,
+			first_utm_content,
+			first_utm_id,
+			first_utm_term,
+			last_utm_campaign,
+			last_utm_source,
+			last_utm_medium,
+			last_utm_content,
+			last_utm_id,
+			last_utm_term,
+			first_page_href,
+			first_page_hostname,
+			first_page_pathname,
+			last_page_href,
+			last_page_hostname,
+			last_page_pathname)
+		values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23)`,
+			[
+				trackedUserId,
+				body.companyId,
+				body.city,
+				body.region,
+				body.country,
+				body.marketingParams.utm_campaign,
+				body.marketingParams.utm_source,
+				body.marketingParams.utm_medium,
+				body.marketingParams.utm_content,
+				body.marketingParams.utm_id,
+				body.marketingParams.utm_term,
+				body.marketingParams.utm_campaign,
+				body.marketingParams.utm_source,
+				body.marketingParams.utm_medium,
+				body.marketingParams.utm_content,
+				body.marketingParams.utm_id,
+				body.marketingParams.utm_term,
+				firstPage,
+				hostname,
+				pathname,
+				firstPage,
+				hostname,
+				pathname,
+			]
+		);
+
+		await client.query(`INSERT INTO pseudo (id, tracked_user_id, created_at) VALUES ($1, $2, TO_TIMESTAMP($3 / 1000.0))`, [
+			body.pseudoId,
+			trackedUserId,
+			timestamp,
+		]);
+	}
+
+	if (body.shouldCreateSession) {
+		await client.query(
+			`INSERT INTO session (id, pseudo_id, company_id, first_page_href, first_page_hostname, first_page_pathname, referrer_href, referrer_hostname, referrer_pathname, city, country, region, device_brand, device_model, device_type, device_os, device_os_version, browser, browser_version, utm_campaign, utm_source, utm_medium, utm_content, utm_id, utm_term) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)`,
+			[
+				body.sessionId,
+				body.pseudoId,
+				body.companyId,
+				firstPage,
+				hostname,
+				pathname,
+				body.referrer,
+				referrerHostname,
+				referrerPathname,
+				body.city,
+				body.country,
+				body.region,
+				body.device.vendor,
+				body.device.model,
+				body.device.type,
+				body.os.name,
+				body.os.version,
+				body.browser.name,
+				body.browser.version,
+				body.marketingParams.utm_campaign,
+				body.marketingParams.utm_source,
+				body.marketingParams.utm_medium,
+				body.marketingParams.utm_content,
+				body.marketingParams.utm_id,
+				body.marketingParams.utm_term,
+			]
+		);
+	}
+
+	ctx.waitUntil(client.end());
+
+	return Response.json({
+		success: true,
+	});
+});
+
+router.post('/reconcile-pseudo', async (request, env, ctx) => {
+	const body = await request.json();
+
+	const { allPseudos } = body;
+
+	const client = new Client(env.DATABASE_URL);
+	await client.connect();
+
+	const { rows: trackedUsers } = await client.query(
+		`
+		SELECT
+			pseudo.id AS "pseudoId",
+			tracked_user.id AS "trackedUserId",
+			tracked_user.email AS email
+		FROM
+			pseudo
+			LEFT JOIN tracked_user ON pseudo.tracked_user_id = tracked_user.id
+		WHERE
+			pseudo.id IN($1, $2)
+		ORDER BY
+			tracked_user.created_at`,
+		allPseudos
+	);
+
+	const trackedUsersByEmail = _.groupBy(trackedUsers, (trackedUser) => trackedUser.email);
+
+	for (const email in trackedUsersByEmail) {
+		const oldestTrackedUser = trackedUsersByEmail[email][0];
+
+		trackedUsersByEmail[email].slice(1).forEach(async (trackedUser) => {
+			await client.query(`UPDATE pseudo SET tracked_user_id = $1 WHERE id = $2`, [oldestTrackedUser.trackedUserId, trackedUser.pseudoId]);
+
+			// CHECK: Update some fields before deleting the newer tracked user
+			await client.query(`DELETE FROM tracked_user WHERE id = $1`, [trackedUser.trackedUserId]);
+		});
+	}
+
+	ctx.waitUntil(client.end());
+
+	return Response.json({
+		success: true,
+	});
 });
 
 export default { ...router };
